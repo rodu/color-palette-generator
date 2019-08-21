db97d93d329103710098d84f5046e8961c0fb801
rodu <greenheron35@gmail.com>
2019-08-21 17:22:31 +0100

    Removes unnecessary variables route and just uses index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router
+  .get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  })
+  .post('/variables', (req, res, next) => {
+    console.log(req.body);
+
+    res.status(200).send('OK');
+  });
 
 module.exports = router;
